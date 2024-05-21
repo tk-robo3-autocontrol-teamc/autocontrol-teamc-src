c22dfdda374550819175d052933dd871c1feefff
eng-telur4 <eng.telur4@gmail.com>
2024-05-21 14:40:39 +0900

    サンプルコードファイル移動

--- Sample02/fnc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "fnc.h"
-
-double avg(double m, double n)
-{
-    double r = (m + n) / 2.0;
-    return r;
-}
--- Sample02/fnc.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FNC_H_
-#define _FNC_H_
-
-double avg(double, double);
-
-#endif // _FNC_H_
